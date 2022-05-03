8dee66b4de4c0159754e551644de8c9f912e6a7d
illia-st <drumin123451@gmail.com>
2022-05-03 20:37:15 +0300

    Добавьте файлы проекта.

---
+++ Laba_BD2/Main_menu.cs
@@ -0,0 +1,12 @@
+using System.Data;
+
+namespace Laba_BD2
+{
+    public partial class Main_menu : Form
+    {
+        public Main_menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
